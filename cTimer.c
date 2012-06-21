f3d92a9b5c1c5a4577f5193e59fca0cf6a439823
Jiang Bian <jbian@uams.edu>
2012-06-20 20:54:42 -0500

    added python3 support, tested on OSX 10.7 64bit

--- cTimer.c
+++ cTimer.c
@@ -6,6 +6,24 @@
  */
 
 #include "Python.h"
+
+#if PY_MAJOR_VERSION >= 3
+	#define MOD_ERROR_VAL NULL
+	#define MOD_SUCCESS_VAL(val) val
+	#define MOD_INIT(name) PyMODINIT_FUNC PyInit_##name(void)
+    #define MOD_DEF(ob, name, doc, methods) \
+        static struct PyModuleDef moduledef = { \
+            PyModuleDef_HEAD_INIT, name, doc, -1, methods, }; \
+        ob = PyModule_Create(&moduledef);
+#else
+	#define MOD_ERROR_VAL
+	#define MOD_SUCCESS_VAL(val)
+	#define MOD_INIT(name) PyMODINIT_FUNC init##name(void)
+    #define MOD_DEF(ob, name, doc, methods) \
+        ob = Py_InitModule3(name, methods, doc);
+#endif
+
+
 #include <time.h>
 
 #ifdef __APPLE__ // OSX-only
@@ -107,8 +125,18 @@ static PyMethodDef TimerMethods[] = {
 	{NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
-PyMODINIT_FUNC
-initcTimer(void) {
+
+MOD_INIT(cTimer)
+{
 	addToArray(convertTsToDouble(gettime()));
-	(void) Py_InitModule3("cTimer", TimerMethods, "A high precision timer.");
+	PyObject *m;
+
+    MOD_DEF(m, "cTimer", "A high precision timer.",
+            TimerMethods)
+            
+    if (m == NULL)
+        return MOD_ERROR_VAL;
+    
+    return MOD_SUCCESS_VAL(m);
+	//(void) Py_InitModule3("cTimer", TimerMethods, "A high precision timer.");
 }
